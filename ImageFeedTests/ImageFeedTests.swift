7b8932d073df1e7c8450cb6b5b841c7385d12f92
latmren <latmren@yahoo.com>
2026-04-19 19:53:37 +0300

    Initial Commit

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,19 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Dmitry Zherebyatnikov on 19.04.2026.
+//
+
+import Testing
+@testable import ImageFeed
+
+struct ImageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
